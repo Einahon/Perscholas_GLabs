a87bcfee821cb364eda9ad6eb20de4f4ca2d4e34
elie <inahon.elie17@yahoo.com>
2024-11-19 15:27:20 -0500

    Commit Length demo class

--- src/javaStringMethods/LengthDemo.java
+++ src/javaStringMethods/LengthDemo.java
@@ -1,4 +1,16 @@
 package javaStringMethods;
 
 public class LengthDemo {
+    String str1 = "Java";
+    String str2 = "";
+
+    public void getLength() {
+
+        System.out.println(str1.length());  // 4
+        System.out.println(str2.length());  // 0
+        System.out.println("Java".length());  // 4
+        System.out.println("Java\n".length()); // 5
+        System.out.println("Learn Java".length()); // 10
+    }
+
 }
--- src/javaStringMethods/Main.java
+++ src/javaStringMethods/Main.java
@@ -1,5 +1,10 @@
+package javaStringMethods;
+
 public class Main {
     public static void main(String[] args) {
+
+        LengthDemo ld = new LengthDemo();
+        ld.getLength();
 
 
     }
