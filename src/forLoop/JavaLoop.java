4fec51770b2fb97f3bdfc64c43c89ba8c9951f2f
elie <inahon.elie17@yahoo.com>
2024-11-25 00:11:36 -0500

    For loop in java

---
+++ src/forLoop/JavaLoop.java
@@ -0,0 +1,9 @@
+package forLoop;
+
+public class JavaLoop {
+    public static void main(String[] args) {
+        LoopDemoOne loopDemoOne = new LoopDemoOne();
+        loopDemoOne.printingText();
+
+    }
+}
---
+++ src/forLoop/LoopDemoOne.java
@@ -0,0 +1,12 @@
+package forLoop;
+
+public class LoopDemoOne {
+    public void printingText(){
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java is fun");
+
+        }
+    }
+}
