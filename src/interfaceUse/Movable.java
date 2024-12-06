914c7e9c0d9949c15c0c37489e118d8fa3db50a6
elie <inahon.elie17@yahoo.com>
2024-12-05 23:56:09 -0500

    How to use Interface

---
+++ src/interfaceUse/Movable.java
@@ -0,0 +1,10 @@
+package interfaceUse;
+
+public interface Movable {
+    void moveUp();    // "public" and "abstract" by default
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+    String getCoordinate();
+
+}
