7c90c3ae4e5a5bd29e5914ec6d3f588493599cfb
elie <inahon.elie17@yahoo.com>
2024-12-11 21:13:37 -0500

    HashSet and TreeSet Processing

---
+++ src/set/HashSet/HashSetExample.java
@@ -0,0 +1,21 @@
+package set.HashSet;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet<Integer> evenNumber = new HashSet<>();
+        // Using add() method
+        evenNumber.add(2);
+        evenNumber.add(4);
+        evenNumber.add(6);
+        System.out.println("set.HashSet: " + evenNumber);
+
+        HashSet<Integer> numbers = new HashSet<>();
+        // Using addAll() method
+        numbers.addAll(evenNumber);
+        numbers.add(5);
+        System.out.println("New set.HashSet: " + numbers);
+
+    }
+}
